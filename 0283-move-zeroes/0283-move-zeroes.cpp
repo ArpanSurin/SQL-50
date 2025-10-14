b8ad2b675a0bc4a6e796ca6d15c62253c0d65f76
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-10-14 17:31:24 +0530

    Time: 0 ms (100%), Space: 24 MB (20.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,8 +3,10 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = 0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] != 0){ 
-                swap(nums[i], nums[n]);
+            if(nums[i] != 0){
+                if(i != n){
+                    swap(nums[i], nums[n]);
+                }
                 n++;
             }
         }
