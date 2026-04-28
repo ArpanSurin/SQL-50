8a226331ddcd75d234e5573e6f7bc3901d599104
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2026-04-28 20:51:51 +0530

    Time: 0 ms (100%), Space: 22.9 MB (52.62%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* temp = head;
+        ListNode* nextNode = nullptr;
+        ListNode* prevNode = nullptr;
+        while(temp != nullptr){
+            nextNode = temp->next;
+            temp->next = prevNode;
+            prevNode = temp;
+            temp = nextNode;
+        }
+        return prevNode;
+    }
+
+    void reorderList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* slow = temp;
+        ListNode* fast = temp;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* first = head;
+        ListNode* second = reverseList(slow->next);
+        slow->next = nullptr;
+        
+        while(second){
+            ListNode *tmp1 = first->next, *tmp2 = second->next;
+            first->next = second;
+            second->next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+};
