0e156ac754deecb3adb4606dec5aa0ffb59705e2
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2026-04-30 11:46:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = INT_MAX;
+        int buy = prices[0];
         int max_profit = 0;
-        for(int i=0; i<prices.size(); i++){
+        for(int i=1; i<prices.size(); i++){
             buy = min(buy, prices[i]);
             max_profit = max(max_profit, prices[i] - buy);
         }
